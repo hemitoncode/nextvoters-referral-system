addafeb4f16520cb2db3f75c4d8b48923c43c59d
Hemit Patel <hemitvpatel@gmail.com>
2025-09-04 20:11:00 -0400

    make it more responsive

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -29,7 +29,7 @@ const Share = () => {
               />
               
               {/* HTML Text Overlay with Tailwind Absolute Positioning */}
-              <p className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-2xl font-extrabold text-yellow-300 text-center">
+              <p className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-xl sm:text-2xl font-extrabold text-yellow-300 text-center">
                 {referralCode}
               </p>
             </div>
